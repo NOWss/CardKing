c31a69cd6f3831ad40ba7e38b97d6cf324b0ea54
Now <138880448+NOWss@users.noreply.github.com>
2025-02-20 14:42:39 +0800

    test

---
+++ CardCMS/runtime/complile/b60a00fb2c90eeb2ed82b93865b8cc7e.php
@@ -0,0 +1,332 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<title>{pboot:sitetitle}</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- Favicon -->
+
+		<!-- bootstrap CSS -->
+		<link rel="stylesheet" href="static/css/bootstrap.min.css" type="text/css" media="all">
+		<!-- carousel CSS -->
+		<link rel="stylesheet" href="static/css/owl.carousel.min.css" type="text/css" media="all">
+		<!-- animate CSS -->
+		<link rel="stylesheet" href="static/css/animate.css" type="text/css" media="all">
+		<!-- animated-text CSS -->
+		<link rel="stylesheet" href="static/css/animated-text.css" type="text/css" media="all">
+		<!-- font-awesome CSS -->
+		<link rel="stylesheet" href="static/css/all.min.css" type="text/css" media="all">
+		<!-- font-flaticon CSS -->
+		<link rel="stylesheet" href="static/css/flaticon.css" type="text/css" media="all">
+		<!-- theme-default CSS -->
+		<link rel="stylesheet" href="static/css/theme-default.css" type="text/css" media="all">
+		<!-- meanmenu CSS -->
+		<link rel="stylesheet" href="static/css/meanmenu.min.css" type="text/css" media="all">
+		<!-- transitions CSS -->
+		<link rel="stylesheet" href="static/css/owl.transitions.css" type="text/css" media="all">
+		<!-- venobox CSS -->
+		<link rel="stylesheet" href="static/css/venobox.css" type="text/css" media="all">
+
+		<!-- bootstrap icons -->
+		<link rel="stylesheet" href="static/css/bootstrap-icons.css" type="text/css" media="all">
+
+		<!-- Main Style CSS -->
+		<link rel="stylesheet" href="static/css/style.css" type="text/css" media="all">
+		<!-- responsive CSS -->
+		<link rel="stylesheet" href="static/css/responsive.css" type="text/css" media="all">
+
+		<link rel="stylesheet" href="static/css/dist/newStyle.css" type="text/css" media="all">
+		<!-- modernizr js -->
+		<script src="static/js/modernizr-3.5.0.min.js"></script>
+	</head>
+
+	<body>
+		<!--==================================================-->
+<!-- Start itpro Main Menu Area -->
+<!--==================================================-->
+<div id="sticky-header" class="itpro_nav_manu">
+  <div class="container" style="max-width: 100vw;">
+    <div class="row btm-bg align-items-center">
+      <div class="col-lg-3">
+        <div class="logo">
+          <a class="logo_img" href="" title="WasData">
+            <img src="static/picture/logo.png" alt="logo" width="170">
+          </a>
+          <a class="main_sticky" href="" title="WasData">
+            <img src="static/picture/logo1.png" alt="logo" width="170">
+          </a>
+        </div>
+      </div>
+      <div class="col-lg-6 pl-0 pr-0">
+        <nav class="itpro_menu">
+          <ul class="nav_scroll">
+            <li><a href="{pboot:sitedomain}#home">首页</a></li>
+            <li><a href="{pboot:sitedomain}#service">付款卡特点</a></li>
+            <li><a href="{pboot:sitedomain}#product">使用场景</a></li>
+            <li><a href="{pboot:sitedomain}#youshi">安全</a></li>
+            <li><a href="{pboot:sitedomain}#yunwei">我们的团队</a></li>
+            <li><a href="{pboot:sitedomain}#news">最新资讯</a></li>
+          </ul>
+        </nav>
+      </div>
+      <div class="col-lg-3 pl-0 pr-0">
+        <div class="header_right_btn">
+          <div class="img-box">
+            <img src="static/images/Sign in.png">
+          </div>
+          <div class="text-box">
+            <a href="#">Sign up  |  Log in</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- itpro Mobile Menu Area -->
+<div class="mobile-menu-area sticky d-sm-block d-md-block d-lg-none">
+  <div class="mobile-menu">
+    <nav class="itpro_menu">
+      <ul class="nav_scroll">
+        <li><a href="{pboot:sitedomain}#home">首页</a></li>
+        <li><a href="{pboot:sitedomain}#product">付款卡特点</a></li>
+        <li><a href="{pboot:sitedomain}#service">使用场景</a></li>
+        <li><a href="{pboot:sitedomain}#youshi">安全</a></li>
+        <li><a href="{pboot:sitedomain}#contact">我们的团队</a></li>
+        <li><a href="{pboot:sitedomain}#contact">最新资讯</a></li>
+      </ul>
+    </nav>
+  </div>
+</div>
+<!--==================================================-->
+<!-- End itpro Main Menu Area -->
+<!--==================================================-->
+		<!--==================================================-->
+		<!-- Start itpro case study Area -->
+		<!--==================================================-->
+		<div class="blog_area " id="news" style="margin: 100px 0;">
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="col-lg-12" style="margin-bottom: 30px;">
+						<div class="col-lg-12 pl-0 pr-0">
+							<div class="itpro-section-title">
+								<h2 style="font-size: 44px; text-align: center;">最新资讯</h2>
+								<div class="tabs">
+									{pboot:nav num=10 parent=0}
+									<a href="[nav:link]" class="tab-link">
+										<div class="item">
+											<div>所有文章 [nav:rows]</div>
+											<!-- <div>篇文章</div> -->
+										</div>
+									</a>
+									{pboot:if([nav:soncount]>0)}
+									{pboot:2nav parent=[nav:scode]}
+									<a href="[2nav:link]" class="tab-link">
+										<div class="item">
+											<div>[2nav:name]文章 [2nav:rows]</div>
+											<!-- <div>类 </div> -->
+										</div>
+									</a>
+									{/pboot:2nav}
+									{/pboot:if}
+									{/pboot:nav}
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row grid">
+					{pboot:list num=10 order=sorting}
+					<div class="col-lg-4 col-md-6 grid-item">
+						<div class="new-box">
+							{pboot:if('[list:ico]' != '/static/images/nopic.png')}
+							<div class="top">
+								<div class="img-box">
+									<img src="[list:ico]" alt="">
+								</div>
+							</div>
+							{/pboot:if}
+							<div class="bottom">
+								<div class="text-box">
+									<h4>[list:title]</h4>
+									<p>[list:content drophtml=1 dropblank=1 lencn=60]</p>
+								</div>
+							</div>
+							<a href="[list:link]">
+								<div class="newBtn">点击查看</div>
+							</a>
+						</div>
+					</div>
+					{/pboot:list}
+					<!-- <div class="col-lg-4 grid-item">
+					</div> -->
+				</div>
+				<!-- <div class="col-12">
+  <div class="pagination justify-content-center mt-4">
+    <a href="#" class="active">1</a>
+    <a href="#">2</a>
+    <a href="#"><i class="fas fa-angle-double-right"></i></a>
+  </div>
+</div> -->
+<style>
+  .page-num {
+    font-size: 18px;
+    font-weight: 500;
+    color: #232323;
+    float: left;
+    text-decoration: none;
+    transition: background-color .3s;
+    margin: 0 4px;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+    height: 40px;
+    width: 40px;
+    line-height: 40px;
+    text-align: center;
+    cursor: pointer;
+  }
+
+  .page-num a:hover {
+    color: #fff;
+    background-color: #232323;
+  }
+
+  .page-num-current {
+    z-index: 1;
+    color: #fff !important;
+    background-color: #232323;
+    /* border-color: #36c95f8; */
+  }
+</style>
+{pboot:if({page:rows}>0)}
+<div class="col-12">
+  <nav aria-label="page navigation pagination justify-content-center mt-4" class="my-4">
+    <div class="pagination justify-content-center">
+      <a href="{page:index}"><i class="fas fa-angle-double-left"></i></a>
+      <!-- <a class="page-item page-link" href="{page:pre}">上一页</a> -->
+      {page:numbar}<!-- 数字条，小屏幕时自动隐藏-->
+      <!-- <a class="page-item page-link" href="{page:next}">下一页</a> -->
+      <a href="{page:last}"><i class="fas fa-angle-double-right"></i></a>
+    </div>
+  </nav>
+  {else}
+  <div class="text-center my-5 text-secondary">未查询到任何数据！</div>
+  {/pboot:if}
+</div>
+
+			</div>
+		</div>
+		<!--==================================================-->
+		<!-- End itpro case study Area -->
+		<!--==================================================-->
+		<!-- Start itpro Footer Middle Area -->
+		<!--==================================================-->
+		<div class="footer-middle style-two upper" id="contact">
+  <div class="footer-bottom-area d-flex align-items-center" style="height: 120px;">
+    <div class="container">
+      <div class="row d-flex align-items-center">
+        <div class="col-md-4">
+          <div class="itpro-logo">
+            <a class="logo_thumb" href="/" title="itpro">
+              <img src="static/picture/logo1.png" alt="logo" style="width: 50%;">
+            </a>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div style="color: #fff;">
+            <div style="margin-bottom: 10px;">
+              <i class="bi bi-clock-history"></i>
+              工作时间：9:00AM - 04:00 AM
+            </div>
+            
+              <div>
+                <i class="bi bi-send"></i>
+                Telegram客服：<a href="https://t.me/cardking88">@cardking</a>
+              </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="footer-bottom-content">
+            <div class="footer-bottom-content-copy">
+              <p>{pboot:sitecopyright}<a rel="nofollow">{pboot:siteicp}</a> </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!--==================================================-->
+<!-- End itpro Footer Middle Area -->
+<!--==================================================-->
+		<!--==================================================-->
+		<!-- End itpro Footer Middle Area -->
+		<!--==================================================-->
+
+
+
+
+		<!--==================================================-->
+		<!-- Start scrollup section Area -->
+		<!--==================================================-->
+		<!-- scrollup section -->
+		<div class="scroll-area">
+			<div class="top-wrap">
+				<div class="go-top-btn-wraper">
+					<div class="go-top go-top-button">
+						<i class="fas fa-arrow-up"></i>
+						<i class="fas fa-arrow-up"></i>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--==================================================-->
+		<!-- Start scrollup section Area -->
+		<!--==================================================-->
+
+		<!-- jquery js -->
+		<script src="static/js/jquery-3.6.2.min.js"></script>
+		<script src="static/js/popper.min.js"></script>
+		<!-- bootstrap js -->
+		<script src="static/js/bootstrap.min.js"></script>
+		<!-- carousel js -->
+		<script src="static/js/owl.carousel.min.js"></script>
+		<!-- counterup js -->
+		<script src="static/js/jquery.counterup.min.js"></script>
+		<!-- waypoints js -->
+		<script src="static/js/waypoints.min.js"></script>
+		<!-- wow js -->
+		<script src="static/js/wow.js"></script>
+		<!-- imagesloaded js -->
+		<script src="static/js/imagesloaded.pkgd.min.js"></script>
+		<!-- venobox js -->
+		<script src="static/js/venobox.js"></script>
+
+		<!--  animated-text js -->
+		<script src="static/js/animated-text.js"></script>
+		<!-- venobox min js -->
+		<script src="static/js/venobox.min.js"></script>
+		<!-- isotope js -->
+		<script src="static/js/isotope.pkgd.min.js"></script>
+		<!-- jquery meanmenu js -->
+		<script src="static/js/jquery.meanmenu.js"></script>
+
+		<!-- jquery scrollup js -->
+		<script src="static/js/jquery.scrollUp.js"></script>
+		<!-- theme js -->
+		<script src="static/js/theme.js"></script>
+		<!-- barfiller -->
+		<script src="static/js/jquery.barfiller.js"></script>
+		<script src="static/js/masonry.min.js"></script>
+		<script src="static/js/active.js"></script>
+		<!-- 将字体转换为繁体 -->
+		<script src="static/js/jfzh.js"></script>
+	</body>
+
+</html><?php return array (
+  0 => '//ADMIN/CARDKING/CardCMS/template/html/comm/head.html',
+  1 => '//ADMIN/CARDKING/CardCMS/template/html/comm/page.html',
+  2 => '//ADMIN/CARDKING/CardCMS/template/html/comm/foot.html',
+); ?>
